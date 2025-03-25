5b523c560bbe66af7bbf80fa29b88ba873c88ba9
Robinson777-prog <aiinfoai8@gmail.com>
2025-03-25 11:18:54 -0500

    Update main.py
    Add feeback method to client

--- memory/client/main.py
+++ memory/client/main.py
@@ -3,7 +3,6 @@ import os
 import warnings
 from functools import wraps
 from typing import Any, Dict, List, Optional, Union
-from enum import Enum
 
 import httpx
 
@@ -75,7 +74,7 @@ class MemoryClient:
         Raises:
             ValueError: If no API key is provided or found in the environment.
         """
-        self.api_key = api_key or os.getenv("m0-rutudFpA7Y7C6DJ2ut4xy7IPzG8PoDUXCBeryXF7")
+        self.api_key = api_key or os.getenv("MEM0_API_KEY")
         self.host = host or "https://api.mem0.ai"
         self.org_id = org_id
         self.project_id = project_id
@@ -618,6 +617,25 @@ class MemoryClient:
         capture_client_event("client.delete_webhook", self, {"webhook_id": webhook_id})
         return response.json()
 
+    @api_error_handler
+    def feedback(self, memory_id: str, feedback: Optional[str] = None, feedback_reason: Optional[str] = None) -> Dict[str, str]:
+        VALID_FEEDBACK_VALUES = {"POSITIVE", "NEGATIVE", "VERY_NEGATIVE"}
+
+        feedback = feedback.upper() if feedback else None
+        if feedback is not None and feedback not in VALID_FEEDBACK_VALUES:
+            raise ValueError(f'feedback must be one of {", ".join(VALID_FEEDBACK_VALUES)} or None')
+
+        data = {
+            "memory_id": memory_id,
+            "feedback": feedback,
+            "feedback_reason": feedback_reason
+        }
+
+        response = self.client.post("/v1/feedback/", json=data)
+        response.raise_for_status()
+        capture_client_event("client.feedback", self, data)
+        return response.json()
+
     def _prepare_payload(
         self, messages: Union[str, List[Dict[str, str]], None], kwargs: Dict[str, Any]
     ) -> Dict[str, Any]:
@@ -1007,7 +1025,7 @@ class AsyncMemoryClient:
         feedback = feedback.upper() if feedback else None
         if feedback is not None and feedback not in VALID_FEEDBACK_VALUES:
             raise ValueError(f'feedback must be one of {", ".join(VALID_FEEDBACK_VALUES)} or None')
-            
+
         data = {
             "memory_id": memory_id,
             "feedback": feedback,
